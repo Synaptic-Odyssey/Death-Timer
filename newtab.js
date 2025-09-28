b6d63ed1facab04ba591a618f0233b328d83aa40
Kevin C <108781095+Synaptic-Odyssey@users.noreply.github.com>
2025-09-27 20:37:08 -0700

    newtab js

--- newtab.js
+++ newtab.js
@@ -1,2 +1,28 @@
 // Can have two modes, one fractional years, the other split into years, days, hours, seconds
-//prioritize fractional
+//prioritize fractional for a MVP
+
+const msYear = 365.25 * 24 * 60 * 60 * 1000;
+const intervalMs = 50
+
+chrome.storage.local.get(["diffYears"], function(result) {
+    console.log(result.diffYears);
+
+    untilDeath = result.diffYears;
+
+
+    const timer = document.querySelector("#yearsLeft");
+
+    const intervalId = setInterval(function() {
+
+    untilDeath -= intervalMs/msYear;
+    timer.textContent = untilDeath;
+
+    if (untilDeath <= 0) {
+        clearInterval(intervalId);
+        timer.textContent = "Rip";
+    }   
+
+
+    }, intervalMs);
+});
+
